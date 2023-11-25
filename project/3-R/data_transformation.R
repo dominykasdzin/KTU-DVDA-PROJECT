bed7870411c2147d7264af9ede63646a4baec091
Dominykas Dzingelevičius <dominykas.dzin@gmail.com>
2023-11-25 12:50:20 +0200

    Created train data

--- project/3-R/data_transformation.R
+++ project/3-R/data_transformation.R
@@ -1,3 +1,12 @@
-library(readr)
-read_csv("../1-data/put_your_data_files_here.csv")
+if(!require("tidyverse")) install.packages("tidyverse"); library("tidyverse")
+
+data_sample <- read_csv("../1-data/1-sample_data.csv")
+data_additional <- read_csv("../1-data/3-additional_features.csv")
+data_sample2 <- read_csv("../1-data/2-additional_data.csv")
+
+partial_joined <- full_join(data_sample, data_sample2)
+joined_data <- inner_join(partial_joined, data_additional, by = "id")
 ### save combined file into 1-data directory
+
+write_csv(joined_data, "../1-data/train_data.csv")
+
